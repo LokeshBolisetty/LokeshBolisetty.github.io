58cc7ac59218a2b2e7647af638bafd33cf58a5ae
Lokesh Bolisetty <63059375+LokeshBolisetty@users.noreply.github.com>
2020-06-02 00:49:46 +0530

    Add files via upload

--- MovieManiac/index.js
+++ MovieManiac/index.js
@@ -2,61 +2,81 @@
 //const fs = require('fs');
 //const path = require('path')
 const link = ' http://www.omdbapi.com/?i=tt3896198&apikey=e602044f&r=json&s=';
+const link1 = 'http://www.omdbapi.com/?i=';
+const link2 = '&apikey=e602044f&r=json';
 function Search() {
-    let search = document.getElementById('search').value;
-    movies(search);
+  let search = document.getElementById('search').value;
+  movies(search);
 }
 
 function movies(search) {
-    axios
-      .get(link + search)
-      .then((res) => {
-        console.log(res);
-        //let movieitem= res;
-        let movies = res.data.Search;
-        movies.forEach(movieitem => {
-            document.getElementById('desc').innerHTML+=`<p>Title: ${movieitem.Title}<br>Year: ${movieitem.Year}<br>IMDb ID:${movieitem.imdbID}<br><hr></p>`;
-        });
-        //document.getElementById('desc').innerHTML=`<p>Title: ${movieitem.data.Title}<br>Actors: ${movieitem.data.Actors}<br>IMDb Rating: ${movieitem.data.Ratings[0].Value}<br>Year of release: ${movieitem.data.Year}<br>Genre: ${movieitem.data.Genre}</p`;
-        showOutput(res);
-      })
+  axios
+    .get(link + search)
+    .then((res) => {
+      console.log(res);
+      //let movieitem= res;
+      let movies = res.data.Search;
+      movies.forEach(movieitem => {
+
+        document.getElementById('desc').innerHTML += `<div class="lister"><p>Title: ${movieitem.Title}<br>Year: ${movieitem.Year}<br>IMDb ID:${movieitem.imdbID}<br><button type="button" id=${movieitem.imdbID} onClick=Click(id) class="btn btn-primary form-control">View More</button><hr></p></div>`;
+      });
+      //document.getElementById('desc').innerHTML=`<p>Title: ${movieitem.data.Title}<br>Actors: ${movieitem.data.Actors}<br>IMDb Rating: ${movieitem.data.Ratings[0].Value}<br>Year of release: ${movieitem.data.Year}<br>Genre: ${movieitem.data.Genre}</p`;
+      showOutput(res);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+}
+function Click(imdb){
+  document.getElementById('desc').innerHTML="";
+  axios
+    .get(link1 + imdb+link2)
+    .then((res) => {
+      console.log(res);
+      let movieitem= res;
+      //let movieitem = res.data.Search;
+     // movies.forEach(movieitem => {
+        document.getElementById('desc').innerHTML += `<form class="clicker"><image src="${movieitem.data.Poster}" height=350px><p>Title: ${movieitem.data.Title}<br>Actors: ${movieitem.data.Actors}<br>IMDb Rating: ${movieitem.data.Ratings[0].Value}<br>Year of release: ${movieitem.data.Year}<br>Genre: ${movieitem.data.Genre}</p></form>`;
+
+      
+    })
       .catch((err) => {
         console.log(err);
       });
-      
-  }
+}
   //function showOutput(res) {
     //document.getElementById(res).innerHTML=`${res.data.Actors}`;
-  
-  
-  /*function showOutput(res) {
-    document.getElementById('res').innerHTML = `
-    <div class="card card-body mb-4">
-      <h5>Status: ${res.data.Title}</h5>
+
+
+/*function showOutput(res) {
+  document.getElementById('res').innerHTML = `
+  <div class="card card-body mb-4">
+    <h5>Status: ${res.data.Title}</h5>
+  </div>
+  <div class="card mt-3">
+    <div class="card-header">
+      Headers
     </div>
-    <div class="card mt-3">
-      <div class="card-header">
-        Headers
-      </div>
-      <div class="card-body">
-        <pre>${JSON.stringify(res.headers, null, 2)}</pre>
-      </div>
+    <div class="card-body">
+      <pre>${JSON.stringify(res.headers, null, 2)}</pre>
     </div>
-    <div class="card mt-3">
-      <div class="card-header">
-        Data
-      </div>
-      <div class="card-body">
-        <pre>${JSON.stringify(res.data, null, 2)}</pre>
-      </div>
+  </div>
+  <div class="card mt-3">
+    <div class="card-header">
+      Data
     </div>
-    <div class="card mt-3">
-      <div class="card-header">
-        Config
-      </div>
-      <div class="card-body">
-        <pre>${JSON.stringify(res.config, null, 2)}</pre>
-      </div>
+    <div class="card-body">
+      <pre>${JSON.stringify(res.data, null, 2)}</pre>
     </div>
-  `;
-  }*/
+  </div>
+  <div class="card mt-3">
+    <div class="card-header">
+      Config
+    </div>
+    <div class="card-body">
+      <pre>${JSON.stringify(res.config, null, 2)}</pre>
+    </div>
+  </div>
+`;
+}*/
